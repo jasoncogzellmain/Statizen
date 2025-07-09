f62b9316dc415d10029a4490b92507e2658a64c9
Jason Cogzell <jasoncogzell@gmail.com>
2025-07-09 12:21:18 -0600

    Added version to footer

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import pkg from './package.json';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -12,5 +13,8 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  define: {
+    __APP_VERSION__: JSON.stringify(pkg.version)
   },
 });
